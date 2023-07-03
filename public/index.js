67ebc650be6b65336108b52ba076b11751db550b
anthony163 <anthony@techpods.co>
2023-07-03 17:02:13 +0300

    Add fixed size to the editor

--- public/index.js
+++ public/index.js
@@ -1,7 +1,21 @@
 import { CodeMirror } from './wisp.editor.bundle.js'
-
 ;(() => {
-  const editor = CodeMirror(document.getElementById('editor-container'), {})
+  const editor = CodeMirror(document.getElementById('editor-container'), {}),
+    RATIO_Y = 1
+
+  editor.focus()
+
+  window.addEventListener('resize', () => {
+    const bouds = document.body.getBoundingClientRect(),
+      width = bouds.width,
+      height = bouds.height
+
+    editor.setSize(width, (height - 60) * RATIO_Y)
+    consoleEditor.setSize(width - 80, 40)
+  })
+  const bounds = document.body.getBoundingClientRect()
+  editor.setSize(bounds.width, (bounds.height - 60) * RATIO_Y)
+
   const console = document.getElementById('console')
   // const run = document.getElementById('run');
   const socket = io()
@@ -16,7 +30,7 @@ import { CodeMirror } from './wisp.editor.bundle.js'
     if (e.key && e.key.toLowerCase() === 's' && (e.ctrlKey || e.metaKey)) {
       e.preventDefault()
       e.stopPropagation()
-      socket.emit('eval', editor.getValue().trim() + '\n')
+      socket.emit('eval', editor.getValue().trim())
     } else if (e.key === 'Escape') {
       e.preventDefault()
       e.stopPropagation()
